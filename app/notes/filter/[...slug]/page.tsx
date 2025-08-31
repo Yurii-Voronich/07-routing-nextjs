89c4ac450c0d2c9a8e42caf385bf76af78c3df27
Yurii <voronich97@gmail.com>
2025-08-31 14:49:37 +0300

    fix

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClientPage from "./notes.client";
+import NotesClientPage from "./Notes.client";
 
 interface NotesProps {
   params: Promise<{ slug: string[] }>;
